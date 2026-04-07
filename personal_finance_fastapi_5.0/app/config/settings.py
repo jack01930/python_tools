962fa6b84c5d412b73418d6aa4812a04347173ea
Luke <2140469539@qq.com>
2026-04-07 14:53:01 +0800

    feat(config): 新增环境配置模块，基于Pydantic Settings封装JWT密钥/算法/过期时间配置，支持.env文件读取与默认值兜底

---
+++ personal_finance_fastapi_5.0/app/config/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+#当前SECRET_KEY/ALGORITHM等字段只需要 “类型正确”（如str/int），Pydantic BaseSettings通过类型注解就能完成基础校验，无需额外的Field
+class Settings(BaseSettings): 
+    SECRET_KEY:str=os.getenv("SECRET_KEY","Z(RJ*b]Y77eayQfs(VETLmrQ-{%1wLJq") #读取环境变量，无则使用默认值
+    ALGORITHM:str="HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES:int=30
+
+settings=Settings()
